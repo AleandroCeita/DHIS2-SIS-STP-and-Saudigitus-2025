38ffb7bd9b38ba3003c7d8c2fb84504923260232
aleandroceita <aleandroaly@gmail.com>
2025-08-04 11:47:14 +0000

    Added text align in my h1

--- src/Components/Sis.tsx
+++ src/Components/Sis.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Sis() {
   return (
     <>
-      <h1 style={{ backgroundColor: 'lightblue'}}>Olá Sis, tudo bem</h1>
+      <h1 style={{ backgroundColor: 'lightblue', textAlign:'right'}}>Olá Sis, tudo bem</h1>
       <p>Sis está no Top</p>
     </>
   )
