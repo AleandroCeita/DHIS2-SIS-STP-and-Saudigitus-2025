7c4ba114384cba375f0df049b6fc2259aadba00e
aleandroceita <aleandroaly@gmail.com>
2025-08-04 16:13:39 +0000

    Update component Sis

--- src/Components/Sis.tsx
+++ src/Components/Sis.tsx
@@ -5,6 +5,7 @@ export function Sis() {
     <>
       <h1 style={{ backgroundColor: 'lightblue', textAlign:'right'}}>Olá Sis, tudo bem</h1>
       <p>Sis está no Top</p>
+      <h2>Welcome Aleandro</h2>
     </>
   )
 }
